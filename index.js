645f6e92cde7dac819c2fd34adc63f249cc3a49b
Thomas Urban <thomas.urban@cepharum.de>
2018-08-13 00:14:07 +0200

    moving compilation of model definitions to extension hitchy-odem

--- index.js
+++ index.js
@@ -53,34 +53,12 @@ module.exports = function( options ) {
 
 			const modelNames = Object.keys( models );
 			const routes = new Map();
-
-			let adapter = ( config.database || {} ).default;
-			if ( adapter ) {
-				if ( !( adapter instanceof Adapter ) ) {
-					Log( "invalid adapter:", adapter );
-					return;
-				}
-			} else {
-				adapter = new MemoryAdapter();
-			}
-
 			const urlPrefix = ( config.model || {} ).urlPrefix || "/api";
-
 
 			for ( let i = 0, numNames = modelNames.length; i < numNames; i++ ) {
 				const name = modelNames[i];
-				const definition = models[name] || {};
-
 				const routeName = name.toLocaleLowerCase();
-				const modelName = definition.name || ( routeName.slice( 0, 1 ).toLocaleUpperCase() + kebapToCamel( routeName ).slice( 1 ) );
-
-				const schema = {};
-
-				mergeAttributes( schema, definition.attributes || {} );
-				mergeComputeds( schema, definition.computeds || {} );
-				mergeHooks( schema, definition.hooks || {} );
-
-				const model = Model.define( modelName, schema, null, adapter );
+				const model = models[name] || {};
 
 				addRoutesOnModel( routes, urlPrefix, routeName, model );
 			}
@@ -89,96 +67,6 @@ module.exports = function( options ) {
 		},
 	};
 };
-
-/**
- * Merges separately defined map of static attributes into single schema
- * matching expectations of hitchy-odem.
- *
- * @param {object} target resulting schema for use with hitchy-odem
- * @param {object<string,function>} source maps names of attributes into either one's definition of type and validation requirements
- * @returns {void}
- */
-function mergeAttributes( target, source ) {
-	const propNames = Object.keys( source );
-
-	for ( let i = 0, numNames = propNames.length; i < numNames; i++ ) {
-		const name = propNames[i];
-		const attribute = source[name];
-
-		switch ( typeof attribute ) {
-			case "object" :
-				if ( attribute ) {
-					break;
-				}
-
-			// falls through
-			default :
-				throw new TypeError( `invalid definition of attribute named "${name}": must be object` );
-		}
-
-		target[name] = attribute;
-	}
-}
-
-/**
- * Merges separately defined map of computed attributes into single schema
- * matching expectations of hitchy-odem.
- *
- * @param {object} target resulting schema for use with hitchy-odem
- * @param {object<string,function>} source maps names of computed attributes into the related computing function
- * @returns {void}
- */
-function mergeComputeds( target, source ) {
-	const propNames = Object.keys( source );
-
-	for ( let i = 0, numNames = propNames.length; i < numNames; i++ ) {
-		const name = propNames[i];
-		const computer = source[name];
-
-		switch ( typeof computer ) {
-			case "function" :
-				break;
-
-			default :
-				throw new TypeError( `invalid definition of computed attribute named "${name}": must be a function` );
-		}
-
-		target[name] = computer;
-	}
-}
-
-/**
- * Merges separately defined map of lifecycle hooks into single schema matching
- * expectations of hitchy-odem.
- *
- * @param {object} target resulting schema for use with hitchy-odem
- * @param {object<string,(function|function[])>} source maps names of lifecycle hooks into the related callback or list of callbacks
- * @returns {void}
- */
-function mergeHooks( target, source ) {
-	const propNames = Object.keys( source );
-
-	for ( let i = 0, numNames = propNames.length; i < numNames; i++ ) {
-		const name = propNames[i];
-		let hook = source[name];
-
-		if ( typeof hook === "function" ) {
-			hook = [hook];
-		}
-
-		if ( !Array.isArray( hook ) ) {
-			throw new TypeError( `invalid definition of hook named "${name}": must be a function or list of functions` );
-		}
-
-		for ( let hi = 0, numHooks = hook.length; hi < numHooks; hi++ ) {
-			if ( typeof hook[hi] !== "function" ) {
-				throw new TypeError( `invalid definition of hook named "${name}": not a function at index #${hi}` );
-			}
-		}
-
-		target[name] = hook;
-	}
-}
 
 /**
  * Adds routes handling common requests related to selected model.
